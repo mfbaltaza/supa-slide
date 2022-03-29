231cfd4ccbce08eb857f406500c04fb22438bff0
Juan Araujo <araujogreydiego@gmail.com>
2022-03-29 15:07:35 -0400

    Fix tailwindcss and adds navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { supabase } from "../api";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(async () =>
+      checkUser()
+    );
+    checkUser();
+    return () => {
+      authListener?.unsubscribe();
+    };
+  }, []);
+
+  async function checkUser() {
+    const user = await supabase.auth.user();
+    setUser(user);
+  }
+
+  return (
+    <div>
+      <nav className="p-6 border-b border-gray-300">
+        <Link href="/" passHref>
+          <span className="mr-6 cursor-pointer">Home</span>
+        </Link>
+        {user && (
+          <Link href="/create-post" passHref>
+            <span className="mr-6 cursor-pointer">Create Post</span>
+          </Link>
+        )}
+        <Link href="/profile" passHref>
+          <span className="mr-6 cursor-pointer">Profile</span>
+        </Link>
+      </nav>
+      <div className="py-8 px-16">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">the jungle!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
